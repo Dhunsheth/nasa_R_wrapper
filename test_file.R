7c86dae083355f963fd3fe12d7024688bb29e841
Jacob Rosen <jimr95>
2024-02-04 13:24:06 -0800

    added test cases

--- test_file.R
+++ test_file.R
@@ -49,3 +49,34 @@ test_that("get_neo returns a ggplot object for a valid date range", {
   result <- get_neo('2023-01-01', '2023-01-07')
   expect_true(inherits(result, 'gg'))
 })
+
+source("epic_wrapper.R")
+
+# get_metadata test cases
+test_that("Invalid API response returns error message", {
+  data <- get_metadata('notarealtype', '2023-01-07')
+  expect_equal(response, 'Response status code not 200')
+})
+
+test_that("Valid API response returns metadata", {
+  data <- get_metadata('natural', '2023-01-07')
+  expect_equal(typeof(data), 'list')
+  expect_equal(typeof(data$image), 'character')  
+})
+
+# get_urls test cases
+test_that("EPIC url list works as expected for natural", {
+  url_list <- get_image_urls('natural', '2023-01-07')
+  expect_equal(url_list[1], 'https://epic.gsfc.nasa.gov/archive/natural/2023/01/07/png/epic_1b_20230107004554.png')
+})
+
+test_that("EPIC url list works as expected for cloud", {
+  url_list <- get_image_urls('cloud', '2023-01-07')
+  expect_equal(url_list[1], "https://epic.gsfc.nasa.gov/archive/cloud/2023/01/07/png/epic_cloudfraction_20230107004554.png")
+})
+
+#plot_centroids test case
+test_that('plot_centroids returns a leaflet plot', {
+  centroid_plot <- get_centroids('natural', '2023-01-07')
+  expect_is(centroid_plot, 'leaflet')
+})
